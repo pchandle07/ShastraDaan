8a70e9d3987ad1a94f0c9a9dd94fe6fb5286b2bf
prathamesh chandle <54568056+pchandle07@users.noreply.github.com>
2021-12-19 12:05:49 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const BASE_API_URL = 'http://shastradaan.ap-south-1.elasticbeanstalk.com/shastradaan';
 
-export const RAZORPAY_PAYMENT_KEY_ID = "rzp_test_P6BGdE4VL5vWrU";
+export const RAZORPAY_PAYMENT_KEY_ID = "rzp_live_pBWB8jeSKHFY3Y";
 
